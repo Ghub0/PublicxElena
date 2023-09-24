57e8f686aa7bf1288f20d668e6cd020474f9ac19
Ghub0 <136447110+Ghub0@users.noreply.github.com>
2023-09-24 21:23:06 +0530

    start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/searchingshiv/mirrorbot.git /mirrorbot
+  git clone https://github.com/Ghub0/auto.git /mirrorbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /mirrorbot
