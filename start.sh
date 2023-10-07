2da4b2c81f5cdbd3d2e7698406c725e91ff74312
Ghub0 <136447110+Ghub0@users.noreply.github.com>
2023-10-07 18:43:27 +0530

    start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Ghub0/auto.git /Auto
+  git clone https://github.com/Ghub0/Auto.git /Auto
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Auto
 fi
-cd /mirrorbot
+cd /Auto
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
