a3d6a86220abf3996bbb87da57c6da3ffd9bdb74
ROCKIND <136447110+ROCKIND@users.noreply.github.com>
2024-03-19 13:40:18 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -49,9 +49,9 @@ REACT = ["🔥", "❤️", "😍", "⚡"]
 
 @Client.on_message(filters.group | filters.private & filters.text & filters.incoming)
 async def give_filter(client, message):
-        await message.react(emoji=random.choice(DS_REACT))
     if message.chat.id != SUPPORT_CHAT_ID:
         manual = await manual_filters(client, message)
+        await message.react(emoji=random.choice(DS_REACT))
         if manual == False:
             settings = await get_settings(message.chat.id)
             try:
