8d4f656430c0c176e6dd8f604afd50a5c5d17b71
ROCKIND <136447110+ROCKIND@users.noreply.github.com>
2024-08-04 21:55:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,7 +13,7 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '25833520'))
+API_ID = int(environ.get('API_ID', 25833520))
 API_HASH = environ.get('API_HASH', '7d012a6cbfabc2d0436d7a09d8362af7')
 BOT_TOKEN = environ.get('BOT_TOKEN', "6528500602:AAEnM7fXJTqSMvbtbc9B9qADTxOBYAJmyQk")
 
